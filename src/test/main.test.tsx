b74195920e47e78a42539f9fdb607e8a894b82d6
Donald Wu <yeukfei02@gmail.com>
2020-04-15 14:51:13 +0800

    feature/create-project-structure-#1: add more test case

--- src/test/main.test.tsx
+++ src/test/main.test.tsx
@@ -4,13 +4,29 @@ import Adapter from "enzyme-adapter-react-16";
 import { shallow, mount } from "enzyme";
 
 import MainPage from '../components/mainPage/MainPage';
+import DisplayResult from '../components/displayResult/DisplayResult';
+import StolenBikeDetails from '../components/stolenBikeDetails/StolenBikeDetails';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
 
 describe("main.test", () => {
   describe("render test", () => {
     it("MainPage", () => {
       shallow(<MainPage />);
+    });
+
+    it("DisplayResult", () => {
+      shallow(<DisplayResult />);
+    });
+
+    it("StolenBikeDetails", () => {
+      shallow(<StolenBikeDetails />);
     });
   });
 });
